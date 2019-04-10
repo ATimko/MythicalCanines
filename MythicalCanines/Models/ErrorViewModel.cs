6c7421c7ae7217e28c45ce7aff48bba88d96f191
ATimko <ashleytimko777@gmail.com>
2019-04-10 14:01:14 -0700

    Add project files.

---
+++ MythicalCanines/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MythicalCanines.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
